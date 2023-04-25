e624b5808cad3168026a77729758c8841dfdf991
xkasiaaa3 <104648382+xkasiaaa3@users.noreply.github.com>
2023-04-25 17:25:55 +0200

    add simple compass

---
+++ Assets/Scripts/SimpleCompass.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCompass : MonoBehaviour
+{
+    public Transform player;
+    Vector3 vector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vector.z = player.eulerAngles.y;
+        transform.localEulerAngles = vector;
+    }
+}
